37fbe593bf176206dfae990a9f8002db04ab688c
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-08-03 17:40:05 +0100

    chore

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+# Check for Homebrew, Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
